ccdc1f21957b3cefe8aff318099828a07cfbc577
colin.allen <colin@allen.org>
2018-03-23 23:35:36 -0400

    GTFS hello world

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,24 @@ var segmentLenghtUniform = distanceBtwnStations / 5px * minute;
 
 var lineLength = sum of station * sum of segmentLenghtUniform
 // todo factory for all the different lines
+
+
+
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+
+var requestSettings = {
+  method: 'GET',
+  url: 'URL OF YOUR GTFS-REALTIME SOURCE GOES HERE',
+  encoding: null
+};
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
